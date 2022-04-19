00417409da341761e69551c13b1f84ffbda1d2be
semen603089 <semen603089@mail.ru>
2022-04-19 20:34:38 +0300

    Fixed

--- parser/main.py
+++ parser/main.py
@@ -1,9 +1,19 @@
 import parser
-import sympy
 from sympy.abc import a, b, y
+import sympy.abc
+
+
+def to_latex(data: list[str]):
+    for i in range(len(data)):
+        temp = data[i].replace('\\', '')
+        data[i] = temp
+    return data
+
 
 if __name__ == '__main__':
     parse_obj: parser.Parser = parser.Parser("ex.tex")
-    for row in parse_obj.parse():
-        expr = sympy.Eq(*map(sympy.sympify, row.split("=")))
+    data = parse_obj.parse()
+    data = to_latex(data)
+    for row in data:
+        expr = sympy.Eq(*map(sympy.sympify, (row.split("="))))
         print(expr.subs([(a, 2), (y, 4), (b, 1)]))
