d4d284641ccb3ccabc639125e9595806968a5c94
Arseniy Uspenskiy <77883678+bziar@users.noreply.github.com>
2022-04-21 00:19:16 +0300

    Update main.py
    Добавил распознавание греческих букв (большую часть времени потратил на составление греческого словаря...)

--- SymPy homework/main.py
+++ SymPy homework/main.py
@@ -1,6 +1,87 @@
 import sympy as sp
 import string
 
+unicode_greek_letters = ['Α', 'α',
+                         'Β', 'β',
+                         'Γ', 'γ',
+                         'Δ', 'δ',
+                         'Ε', 'ε',
+                         'Ζ', 'ζ',
+                         'Η', 'η',
+                         'Θ', 'θ',
+                         'Ι', 'ι',
+                         'Κ', 'κ',
+                         'Λ', 'λ',
+                         'Μ', 'μ',
+                         'Ν', 'ν',
+                         'Ξ', 'ξ',
+                         'Ο', 'ο',
+                         'Π', 'π',
+                         'Ρ', 'ρ',
+                         'Σ', 'σ',
+                         'Τ', 'τ',
+                         'Υ', 'υ',
+                         'Φ', 'φ',
+                         'Χ', 'χ',
+                         'Ψ', 'ψ',
+                         'Ω', 'ω']
+latex_greek_letters = ['\\Alpha',
+                       '\\alpha',
+                       '\\Beta',
+                       '\\beta',
+                       '\\Gamma',
+                       '\\gamma',
+                       '\\Delta',
+                       '\\delta',
+                       '\\Epsilon',
+                       '\\epsilon',
+                       '\\Zeta',
+                       '\\zeta',
+                       '\\Eta',
+                       '\\eta',
+                       '\\Theta',
+                       '\\theta',
+                       '\\Iota',
+                       '\\iota',
+                       '\\Kappa',
+                       '\\kappa',
+                       '\\Lambda',
+                       '\\lambda',
+                       '\\Mu',
+                       '\\mu',
+                       '\\Nu',
+                       '\\nu',
+                       '\\Xi',
+                       '\\xi',
+                       '\\Omicron',
+                       '\\omicron',
+                       '\\Pi',
+                       '\\pi',
+                       '\\Rho',
+                       '\\rho',
+                       '\\Sigma',
+                       '\\sigma',
+                       '\\Tau',
+                       '\\tau',
+                       '\\Upsilon',
+                       '\\upsilon',
+                       '\\Phi',
+                       '\\phi',
+                       '\\Chi',
+                       '\\chi',
+                       '\\Psi',
+                       '\\psi',
+                       '\\Omega',
+                       '\\omega']
+greek_dict = dict(zip(latex_greek_letters, unicode_greek_letters))
+
+
+def check_greek_letter(line: str):
+    for letter in latex_greek_letters:
+        while letter in line:
+            line = line.replace(letter, greek_dict[letter])
+    return line
+
 
 def read_formula(line, border='', border2=''):
     """
@@ -12,6 +93,13 @@ def read_formula(line, border='', border2=''):
     Return SymPy expression for given variable.
     """
 
+    # Find greek letter and change it to the unicode greek letter
+    line = check_greek_letter(line)
+
+    for letter in latex_greek_letters:
+        while letter in line:
+            line = line.replace('\\phi', 'φ')
+
     # Check if there is a fraction and replace \dfrac{...}{...} to .../...
     if '\\frac{' in line:
         i = line.find('\\frac{')
@@ -22,7 +110,8 @@ def read_formula(line, border='', border2=''):
 
     # Check if there is multiplication if variables
     for i in range(len(line) - 1):
-        if line[i] in string.ascii_lowercase and line[i + 1] in string.ascii_lowercase:
+        if (line[i] in string.ascii_lowercase and line[i + 1] in string.ascii_lowercase) or \
+                (line[i] in unicode_greek_letters and line[i + 1] in unicode_greek_letters):
             line = line[:i + 1] + '*' + line[i + 1:]
 
     if border:
@@ -39,7 +128,7 @@ def read_formula(line, border='', border2=''):
 
 
 path = 'latex_example.tex'
-with open(path, 'r') as f:a
+with open(path, 'r') as f:
     for line in f:
         line = line.strip()
         if '$$' in line:
