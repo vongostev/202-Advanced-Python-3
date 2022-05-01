07ea29955a3f279bbd9eaa5e70b714d6953572da
Egisess <duckythunder214@gmail.com>
2022-05-01 21:00:18 +0300

    Many adds

--- main.py
+++ main.py
@@ -1,15 +1,93 @@
 import sympy.abc
 import re
+unicode_greek = ['Α', 'α',
+                 'Β', 'β',
+                 'Γ', 'γ',
+                 'Δ', 'δ',
+                 'Ε', 'ε',
+                 'Ζ', 'ζ',
+                 'Η', 'η',
+                 'Θ', 'θ',
+                 'Ι', 'ι',
+                 'Κ', 'κ',
+                 'Λ', 'λ',
+                 'Μ', 'μ',
+                 'Ν', 'ν',
+                 'Ξ', 'ξ',
+                 'Ο', 'ο',
+                 'Π', 'π',
+                 'Ρ', 'ρ',
+                 'Σ', 'σ',
+                 'Τ', 'τ',
+                 'Υ', 'υ',
+                 'Φ', 'φ',
+                 'Χ', 'χ',
+                 'Ψ', 'ψ',
+                 'Ω', 'ω']
+latex_greek = ['Alpha',
+               'alpha',
+               'Beta',
+               'beta',
+               'Gamma',
+               'gamma',
+               'Delta',
+               'delta',
+               'Epsilon',
+               'epsilon',
+               'Zeta',
+               'zeta',
+               'Eta',
+               'eta',
+               'Theta',
+               'theta',
+               'Iota',
+               'iota',
+               'Kappa',
+               'kappa',
+               'Lambda',
+               'lambda',
+               'Mu',
+               'mu',
+               'Nu',
+               'nu',
+               'Xi',
+               'xi',
+               'Omicron',
+               'omicron',
+               'Pi',
+               'pi',
+               'Rho',
+               'rho',
+               'Sigma',
+               'sigma',
+               'Tau',
+               'tau',
+               'Upsilon',
+               'upsilon',
+               'Phi',
+               'phi',
+               'Chi',
+               'chi',
+               'Psi',
+               'psi',
+               'Omega',
+               'omega']
+
+greek_dict = dict(zip(latex_greek, unicode_greek))
+
+def healer(line: str):
+    for greek_letter in latex_greek:
+        while greek_letter in line:
+            line = line.replace(greek_letter, greek_dict[greek_letter])
 
 
 def parsing():
-    with open('example.tex') as f:
-        data = list(filter(None, f.read().split('\n')))
+    with open('C:\\Users\\Ducky\\OneDrive\\Рабочий стол\\test.txt') as f:
         formulas = list()
-        for item in data:
-            formulas.append(re.findall(r'\$\$(.*?)\$\$', item))
-            formulas.append(re.findall(r'\\begin{equation}(.*?)\\end{equation}', item))
-            #formulas.append(re.findall(r'\$([^$]+)\$', item))
+        for item in f:
+            formulas.append(re.findall(
+                r'\\begin{equation}(.*?)\\end{equation}', item))
+            formulas.append(re.findall(r'\$([^$]+)\$', item))
 
     i = 0
     while i < len(formulas):
@@ -19,16 +97,28 @@ def parsing():
         i += 1
 
     for i in range(len(formulas)):
-        for j in range(len(formulas[i])):
-            temp = formulas[i][j].replace('\\', '')
-            formulas[i][j] = temp
+        formulas[i] = formulas[i][j].replace('\\', '')
+
+        for greek_letter in latex_greek:
+            while greek_letter in formulas[i]:
+                formulas[i] = formulas[i].replace(
+                    greek_letter, greek_dict[greek_letter])
+
+        while (formulas[i].find('frac{') != -1):
+            formulas[i] = formulas[i].replace('frac{', '(')
+            formulas[i] = formulas[i].replace('}{', ')/(')
+            formulas[i] = formulas[i].replace('}', ')')
+
+        if (formulas[i].find('=') != -1):
+            left = sympy.sympify(formulas[i][:formulas[i].find('=')])
+            right = sympy.sympify(formulas[i][(formulas[i].find('=') + 1):])
+            formulas[i] = (f'{left} = {right}')
+        else:
+            formulas[i] = sympy.sympify(formulas[i])
+
     print(formulas)
     return formulas
 
 
 if __name__ == '__main__':
     data = parsing()
-    for i in range(len(data)):
-        for j in range(len(data[i])):
-            expr = sympy.Eq(*map(sympy.sympify, (data[i][j].split("="))))
-            print(expr)
