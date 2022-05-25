64bf8271a22fef3ec578c66379f9563b63a74f55
tiny-engineery <90619504+tiny-engineery@users.noreply.github.com>
2022-05-25 04:11:49 +0300

    Формулы

--- HW.py
+++ HW.py
@@ -105,6 +105,54 @@ def change_greek(line: str):
             line = line.replace(greek_letter, greek_dict[greek_letter])
     return line
 
+def fractions(line: str):
+    """
+    frac{...}{...}
+    Parameters
+    ----------
+    line : str
+    Returns
+    -------
+    line : list of sympyfied left and right sides of equation
+    """
+    while('\\frac{' in line):
+        left_num = line.find('\\frac{')
+        rigth_num = line.find('}', left_num)
+        rigth_denum = line.find('}', rigth_num + 2)
+
+        line = line[:left_num] + line[(left_num + 6):rigth_num] + \
+            ' / ' + line[(rigth_num + 2):rigth_denum] + \
+            line[(rigth_denum + 1):]
+
+    return line
+
+
+def recognize_formula(line: str):
+
+    line = change_greek(line)
+
+    line = fractions(line)
+
+    '''Changing {12ab} to {12*a*b}'''
+    length = len(line)
+    i = 1
+    while(i < length):
+        if (line[i] in all_symbols_string and line[i - 1] in (all_symbols_string + '1234567890')):
+            line = line[:i] + '*' + line[i:]
+            length += 1
+        i += 1
+
+    ''' Finally sympy expressions'''
+    if line.find('=') != -1:
+        left_of_equation = sympy.sympify(line[:line.find('=')])
+        rigth_of_equation = sympy.sympify(line[(line.find('=') + 1):])
+        # print(f'{left_of_equation} = {rigth_of_equation}')
+        return [left_of_equation, rigth_of_equation]
+    else:
+        equation = sympy.sympify(line)
+        return ['', equation]
+        # print(equation)
+
 
 
 
